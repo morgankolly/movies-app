14efcd5667fbe7091a32f03ca56224e6f0f38ad1
@morgankolly <morgankolly@morgankollys-MacBook-Pro.local>
2025-06-18 12:34:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+const parentElement = document.getElementById('movies');
+
+
+async function movie() {
+    const url = 'https://imdb8.p.rapidapi.com/title/v2/get-popular?first=20&country=US&language=en-US';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'b32b594d15msheae9e8c4e22156dp189a95jsn0132b6b8dd53',
+            'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log("result", result)
+        const moviedata = result.data.movies.edges
+        const tvshow = result.data.tv
+        console.log('movieData',moviedata);
+
+        moviedata.forEach(movie => {
+            const item = movie.node;
+            console.log("item", item)
+            const movieElement = document.createElement('div');
+            movieElement.className = 'article';
+            movieElement.innerHTML = `
+             <img class="image" src="${item.primaryImage.url}" />
+              <h2>${item.titleText.text}</h2>
+              <p>${item.plot.plotText.plainText}</p>
+              
+            `;
+      
+            parentElement.appendChild(movieElement);
+          });
+
+
+    } catch (error) {
+        console.error(moviedata);
+    }
+    
+    }
+
+    
+movie();
