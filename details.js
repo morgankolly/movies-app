b1d7a2125137866ffa824c213a41ab5d72143e59
morgankolly <morgankolly5@gmail.com>
2025-07-06 18:12:22 +0300

    modified details.js

--- details.js
+++ details.js
@@ -1,4 +1,8 @@
-const url = 'https://imdb236.p.rapidapi.com/api/imdb/tt0816692';
+const parentElement = document.getElementById('movies');
+
+
+async function movie() {
+const url = 'https://imdb236.p.rapidapi.com/api/imdb/tt0816692/tmdb-id';
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,31 +14,32 @@ const options = {
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
-     console.log("result", result)
-        const moviedata = result.data.movies.edges
-        const tvshow = result.data.tv
-        console.log('movieData',moviedata);
+	console.log("result",result)
+	  console.log('moviedata',moviedata);
 
-         moviedata.forEach(movie => {
-            const item = movie.node;
-             console.log("item", item.id)
+	        moviedata.forEach(movie => {
+            const item = movie.id;
+            
+            console.log("item", item.id)
             const movieElement = document.createElement('div');
             movieElement.className = 'article';
             movieElement.innerHTML = `
-             <img class="image" src="${item.primaryImage.url}" />
-              <h2>${item.certificate.id.ratingReason}</h2>
-              <p>${item.id}</p>
+              <h2>${item.id.id}</h2>
+			  <p>${item.url.url}</p>
+              <p>${item.tmbdId.tmbdId}</p>
               
             `;
       
             parentElement.appendChild(movieElement);
           });
 
+
     } catch (error) {
         console.error(moviedata);
     }
     
-
+}
 
     
 movie();
+
